d7987d1668c5ff1be805efcf81294fa8c6052ee2
zhangdong <zhangdong@zhongjiaxin.com>
2018-12-03 18:38:24 +0800

    初始化

---
+++ src/main/java/com/akuma/Application.java
@@ -0,0 +1,20 @@
+package com.akuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by admin on 2018/11/29.
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
